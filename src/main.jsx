1246076a074e9639e1174b5aa87237037c09cd6d
nickaykays <nick.aykays@gmail.com>
2025-09-22 19:20:39 +0500

    updated main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -4,16 +4,16 @@ import "./index.css";
 import App from "./App.jsx";
 import { QueryClient } from "@tanstack/react-query";
 import { QueryClientProvider } from "@tanstack/react-query";
-import { BrowserRouter as Router } from "react-router-dom";
+import { HashRouter, BrowserRouter as Router } from "react-router-dom";
 
 const queryClient = new QueryClient();
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <QueryClientProvider client={queryClient}>
-      <Router>
+      <HashRouter>
         <App />
-      </Router>
+      </HashRouter>
     </QueryClientProvider>
   </StrictMode>
 );
