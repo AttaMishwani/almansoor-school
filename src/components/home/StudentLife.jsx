e18c5748eac91ee16f8d7cb1a56b042777b21b12
nickaykays <nick.aykays@gmail.com>
2025-09-22 19:44:37 +0500

    completed student life section

--- src/components/home/StudentLife.jsx
+++ src/components/home/StudentLife.jsx
@@ -8,8 +8,20 @@ const StudentLife = () => {
     "https://via.placeholder.com/800x1000?text=Image+Unavailable";
 
   return (
-    <section className="w-full h-[100vh] bg-blue-500 flex justify-center items-center">
-      <div className="w-full h-full flex flex-wrap">
+    <section className="w-full h-[150vh]  bg-blue-500  flex justify-center items-center relative overflow-hidden  ">
+      <svg
+        className="absolute top-0 left-0"
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 1440 280"
+      >
+        <path
+          fill="#F9FAFB"
+          fill-opacity="1"
+          d="M0,0L40,0C80,0,160,0,240,16C320,32,400,64,480,96C560,128,640,160,720,165.3C800,171,880,149,960,128C1040,107,1120,85,1200,80C1280,75,1360,85,1400,90.7L1440,96L1440,0L1400,0C1360,0,1280,0,1200,0C1120,0,1040,0,960,0C880,0,800,0,720,0C640,0,560,0,480,0C400,0,320,0,240,0C160,0,80,0,40,0L0,0Z"
+        ></path>
+      </svg>
+
+      <div className="w-full h-full min-h-[100vh] flex flex-wrap">
         {/* Left big image (40%) */}
         <div className="h-full w-[35%]  ">
           <img
@@ -23,12 +35,12 @@ const StudentLife = () => {
 
         {/* Middle two stacked images (20%) */}
         <div className="w-[30%] h-[100%] flex flex-col justify-center items-center">
-          <div className="w-full h-[50%] text-white poppins-thin  bg-blue-700 flex justify-center items-center p-5">
+          <div className="w-full h-[50%] text-white poppins-thin  bg-blue-600 flex justify-center items-center p-5 pt-[20rem] pb-[8rem]">
             Lorem ipsum dolor sit, amet consectetur adipisicing elit. Non cumque
             libero praesentium accusamus beatae consequuntur necessitatibus
             deserunt ratione quos unde? Neque, rerum velit? Eos dolore placeat
           </div>
-          <div className="w-full h-[50%] text-white poppins-thin  bg-blue-600 flex justify-center items-center p-5">
+          <div className="w-full h-[50%] text-white poppins-thin pb-[20rem] pt-[8rem] border-t-2 border-t-amber-50/45  bg-blue-600 flex justify-center items-center p-5">
             Lorem ipsum dolor sit, amet consectetur adipisicing elit. Non cumque
             libero praesentium accusamus beatae consequuntur necessitatibus
             deserunt ratione quos unde? Neque, rerum velit? Eos dolore placeat
@@ -46,6 +58,18 @@ const StudentLife = () => {
           />
         </div>
       </div>
+
+      <svg
+        className="absolute bottom-0 left-0"
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 1440 280"
+      >
+        <path
+          fill="#F9FAFB"
+          fill-opacity="1"
+          d="M0,0L40,0C80,0,160,0,240,16C320,32,400,64,480,96C560,128,640,160,720,165.3C800,171,880,149,960,128C1040,107,1120,85,1200,80C1280,75,1360,85,1400,90.7L1440,96L1440,320L1400,320C1360,320,1280,320,1200,320C1120,320,1040,320,960,320C880,320,800,320,720,320C640,320,560,320,480,320C400,320,320,320,240,320C160,320,80,320,40,320L0,320Z"
+        ></path>
+      </svg>
     </section>
   );
 };
